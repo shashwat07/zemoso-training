899820ca648b0ab94d75dd892c8ed3a3c9f4a8f4
Shashwat Sinha <shashwat.bits@gmail.com>
2017-08-01 15:28:42 +0530

    Revert "with proper naming"
    This reverts commit 8d32ee72aa05afdd42472b5408c0c3204347c8aa.

--- JavaDay3/task5/Check.java
+++ JavaDay3/task5/Check.java
@@ -4,10 +4,10 @@ public class Check {
 	
 	public static void main(String args[]){
 
-		OuterClass1 outer1 = new OuterClass1();
-		OuterClass2 outer2 = new OuterClass2();
+		A a = new A();
+		X x = new X();
 		
-		outer2.new InnerClass2(outer1, "Success!");
+		X.Y object = x.new Y(a, "Success!");
 	}
 
 }
