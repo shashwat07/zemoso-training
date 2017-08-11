e953464ad90c66827885ff3e593b11c18b147a79
Shashwat Sinha <shashwat.bits@gmail.com>
2017-08-11 17:00:17 +0530

    added comments

--- JavaAssignment2/CheckAlphabets.java
+++ JavaAssignment2/CheckAlphabets.java
@@ -1,12 +1,5 @@
 import java.util.Scanner;
 
-// *******************************************************************
-//	
-// 	Time Complexity -> O(n) : n is the lenth of string.
-//	
-//	Space Complexity -> O(n) : n is the lenth of string.
-//
-// *******************************************************************
 
 public class CheckAlphabets {
 
@@ -14,45 +7,57 @@ public class CheckAlphabets {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		String line = null;
-		System.out.println("Enter the string to check");
-		line = scan.nextLine();
-		boolean arr[] = new boolean[26];
-		boolean flag = true;
-		if(line.length()<26){
-			flag=false;
-		}
-		for(int i=0;i<26;i++){
-			arr[i]=false;
-		}
+		String toCheck = null;
 		
-		for(int i=0;i<line.length();i++){
-			char ch = line.charAt(i);
-			int val = (int) ch;
-			if(val>64 && val <91){
-				arr[val-65]=true;
-			}
-			else if(val>96 && val<123){
-				arr[val-97]=true;
-			}
-		}
-		
-		for(int i=0;i<26;i++){
-			if(arr[i]){
-				flag=true;
-			}
-			else{
-				flag=false;
+		while(true){
+			
+			System.out.println("Enter the string to check, -1 to exit");
+			toCheck = scan.nextLine();
+			if(toCheck.equals("-1")){
 				break;
 			}
+			
+			boolean isPresent[] = new boolean[26];
+			boolean flag = true;
+			
+			// if string is less than 26 characters 
+			if(toCheck.length()<26){
+				flag=false;
+				System.out.println("Does not contain all the alphabets!");
+				continue;
+			}
+			
+			
+			//Assigns boolean value true to indices for corresponding alphabets (0 -> a/A ..... 25 -> z/Z)
+			for(int i=0;i<toCheck.length();i++){
+				char character = toCheck.charAt(i);
+				int val = (int) character;
+				if(val>64 && val <91){
+					isPresent[val-65]=true;
+				}
+				else if(val>96 && val<123){
+					isPresent[val-97]=true;
+				}
+			}
+			
+			// Checks if all the alphabets are present
+			for(int i=0;i<26;i++){
+				if(isPresent[i]){
+					flag=true;
+				}
+				else{
+					flag=false;
+					break;
+				}
+			}
+			if(flag){
+				System.out.println("Contains all the alphabets!");
+			}
+			else{
+				System.out.println("Does not contain all the alphabets!");
+			}
 		}
-		if(flag){
-			System.out.println("Contains all the alphabets!");
-		}
-		else{
-			System.out.println("Does not contain all the alphabets!");
-		}
-		
+		scan.close();
 	}
 
 }
